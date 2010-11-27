8a63e974fc1c0c38cf422efddf31fb2736cb9e2c
Marcus Lindblom <macke@yar.nu>
2010-11-27 02:54:39 +0100

    Add --rebaeif to pull

--- rebaseif.py
+++ rebaseif.py
@@ -9,21 +9,24 @@
 # See http://stackoverflow.com/questions/4086724/how-do-i-check-for-potential-merge-rebase-conflicts-in-mercurial
 # for some discussion on the matter
 
-import mercurial.commands
-
+from mercurial import hg, commands, extensions, cmdutil
+from mercurial.i18n import _
 
 def rebaseif(ui, repo, **opts):
-    '''rebases if no conflicts and merges otherwise
+    '''rebases if there are no file conflicts and merges otherwise.
 
+    See each command's documentation for details.
     '''
+
+    # temporarly replace merge tool to try automatic rebase
+    origmerge = ui.config('ui', 'merge')
+    ui.setconfig('ui', 'merge', 'internal:merge')
 
     import hgext.rebase
 
-    origmerge = ui.config('ui', 'merge')
     try:
-        ui.setconfig('ui', 'merge', 'internal:merge')
-        if hgext.rebase.rebase(ui, repo) == 0:
-            ui.status("rebaseif: successful rebase")
+        hgext.rebase.rebase(ui, repo)
+        ui.status("rebaseif: successful rebase")
         return 0
     except:
         hgext.rebase.rebase(ui, repo, abort=True)
@@ -36,6 +39,45 @@ def rebaseif(ui, repo, **opts):
     mercurial.commands.merge(ui, repo)
 
     return 0
+
+# taken almost in verbatim from rebase extension
+
+def pullrebaseif(orig, ui, repo, *args, **opts):
+    'Call rebaseif after pull if the latter has been invoked with --rebaseif'
+
+    if opts.get('rebaseif'):
+        if opts.get('update'):
+            del opts['update']
+            ui.debug('--update and --rebase are not compatible, ignoring '
+                     'the update flag\n')
+
+        cmdutil.bail_if_changed(repo)
+        revsprepull = len(repo)
+        origpostincoming = commands.postincoming
+        def _dummy(*args, **kwargs):
+            pass
+        commands.postincoming = _dummy
+        try:
+            orig(ui, repo, *args, **opts)
+        finally:
+            commands.postincoming = origpostincoming
+        revspostpull = len(repo)
+        if revspostpull > revsprepull:
+            rebaseif(ui, repo, **opts)
+            branch = repo[None].branch()
+            dest = repo[branch].rev()
+            if dest != repo['.'].rev():
+                # there was nothing to rebase we force an update
+                hg.update(repo, dest)
+    else:
+        orig(ui, repo, *args, **opts)
+
+def uisetup(ui):
+    'Replace pull with a decorator to provide --rebaseif option'
+    entry = extensions.wrapcommand(commands.table, 'pull', pullrebaseif)
+    entry[1].append(('', 'rebaseif', None,
+                     _("rebase or merge working directory to branch head"))
+)
 
 cmdtable = {
     # 'command-name': (function-call, options-list, help-string)
