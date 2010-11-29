f551b95bd76b7a0e21b2eb75bcb23385f71d1abb
Marcus Lindblom <macke@yar.nu>
2010-11-29 17:25:20 +0100

    checkfiles: Allow user to enable hooks in config, add autofix precommithook, use tab_size arg in cmd, more documentaton

--- checkfiles.py
+++ checkfiles.py
@@ -1,17 +1,54 @@
 # Copyright: Marcus Lindblom 2010
 # License: GPLv2+
 #
-# This extension adds a pretxncommit hook that checks files for tabs or trailing whitespace and fails if so
+# This extension provides two hooks and a command for avoiding commits with 
+# tabs or trailing whitespace in specific files.
 #
-# Example usage:
+#
+# == The hooks ==
+#
+# There is a pretxncommit hook for checking for tabs/whitespace, which is useful on a central 
+# repository.
+
+# The second hook runs in pre-commit and automatically fixes any problematic files before commit.
+# (As this actually changes files on disc, you will need to recompile your project after your commit,
+#  but naturally only if there were changes.)
+#
+# If you're not confortable with this kind of magic, use the pretxncommit hook and the command with
+# the --fixup option.
+#
+#
+# == The command == 
+#
+# hg checkfiles [options]
+# 
+# checks changed files in the working directory for tabs or trailing whitespace
+# 
+#     - --verbose shows the location of offending characters in each line
+#     - --quiet hides filenames and only reports summary information
+#     - --debug shows settings and details about each file considered for checking
+# 
+#     If problems are found, the command returns 1, otherwise 0.
+#     If --fixup is given, the return value is always 0 (unless an error occurs).
+#
+# options:
+# 
+#  -f --fixup          fix files by replacing tabs and removing trailing whitespace
+#  -t --tabsize VALUE  set the tab length (default: 8 or checkfiles.tab_size) (default: 4) 
+#
+#
+# == Example usage ==
 #
 # [extensions]
-# pretxncommit = <path>/checkfiles.py
+# checkfiles = /path/to/checkfiles.py
 #
 # [checkfiles]
 # checked_exts = .c .h .cpp .xml .cs .html .js .css .txt .py .nsi .java .aspx .asp .bat .cmd .glsl
 # ignored_files = foo/contains_tabs.txt bar/contains_trailing_ws.txt
 # tab_size = 4
+# commitcheck = True
+# autofixup = True
+
 
 class CheckFiles(object):
     def __init__(self, ui, repo, ctx):
@@ -22,7 +59,7 @@ class CheckFiles(object):
         self.checked_exts = ui.configlist('checkfiles', 'checked_exts',
             default='.c .h .cpp .xml .cs .html .js .css .txt .py .nsi .java .aspx .asp .bat .cmd .glsl')
         self.ignored_files = ui.configlist('checkfiles', 'ignored_files')
-        self.tab_size = int(ui.config('checkfiles', 'tab_size', default='8'))
+        self.tab_size = int(ui.config('checkfiles', 'tab_size', default='4'))
 
         self.ui.debug('checkfiles: checked extensions: %s\n' % ' '.join(self.checked_exts))
         self.ui.debug('checkfiles: ignored files: %s\n' % ' '.join(self.ignored_files))
@@ -101,7 +138,7 @@ class CheckFiles(object):
             if not any(line.isspace() or '\t' in line or line.endswith(' ') for line in lines):
                 continue
 
-            self.ui.status('checkfiles: fixing %s' % file)
+            self.ui.status('checkfiles: fixing %s\n' % file)
 
             with open(os.path.join(self.repo.root, file), 'w') as fileobj:
                 def fixline():
@@ -112,10 +149,20 @@ class CheckFiles(object):
                 fileobj.writelines(fixline())
 
 
+#######################################################################
+
 def checkfiles_hook(ui, repo, node, **kwargs):
-    ui.note('checkfiles: checking commit for tabs or trailing whitespace...\n')
+    ui.status('checkfiles: checking commit for tabs or trailing whitespace...\n')
+
     cf = CheckFiles(ui, repo, repo.changectx(node))
-    cf.check()
+    return cf.check()
+
+def fixup_hook(ui, repo, **kwargs):
+    ui.note('checkfiles: removing tabs and/or trailing whitespace in changed files...\n')
+
+    cf = CheckFiles(ui, repo, repo[None])
+    cf.fixup()
+    return False
 
 def checkfiles_cmd(ui, repo, **opts):
     '''checks changed files in the working directory for tabs or trailing whitespace
@@ -125,10 +172,12 @@ def checkfiles_cmd(ui, repo, **opts):
     - --debug shows settings and details about each file considered for checking
 
     If problems are found, the command returns 1, otherwise 0.
+    If --fixup is given, the return value is always 0 (unless an error occurs).
     '''
 
     ui.note('checkfiles: checking modified files in working directory for tabs or trailing whitespace...\n')
     cf = CheckFiles(ui, repo, repo[None])
+    cf.tab_size = int(opts['tabsize'])
 
     if cf.check():
         if opts['fixup']:
@@ -140,13 +189,18 @@ def checkfiles_cmd(ui, repo, **opts):
 
 #######################################################################
 
+def uisetup(ui):
+    if ui.configbool('checkfiles', 'autofixup', False):
+        ui.setconfig('hooks', 'pre-commit.checkfiles', fixup_hook)
+
 def reposetup(ui, repo):
-    ui.setconfig('hooks', 'pretxncommit.checkfiles', checkfiles_hook)
+    if ui.configbool('checkfiles', 'commitcheck', False):
+        ui.setconfig('hooks', 'pretxncommit.checkfiles', checkfiles_hook)
 
 cmdtable = {
     # 'command-name': (function-call, options-list, help-string)
     'checkfiles': (checkfiles_cmd,
                      [('f', 'fixup', None, 'fix files by replacing tabs and removing trailing whitespace'),
-                      ('t', 'tabsize', None, 'set the tab length (default: 8 or checkfiles.tab_size)')],
+                      ('t', 'tabsize', 4, 'set the tab length')],
                      'hg checkfiles [options]')
 }
